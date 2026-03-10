c43eb7b830667c676ccc7165e03f5330974cd96b
rishita-ops <rishita.sanghavi07@gmail.com>
2026-03-10 14:36:38 +0530

    First Commit

---
+++ code.cpp
@@ -0,0 +1,24 @@
+//Palindrome
+#include <iostream>
+#include <string>   
+using namespace std;
+int main()
+{
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+    int len = str.length();
+    bool isPalindrome = true;
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - i - 1]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+    if (isPalindrome) {
+        cout << "The string is a palindrome." << endl;
+    } else {
+        cout << "The string is not a palindrome." << endl;
+    }
+    return 0;
+}
